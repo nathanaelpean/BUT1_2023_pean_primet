e6521f8b19688e60d5d32e3a7c3be3a6ff59cbf2
Nils <65852707+Swannnils@users.noreply.github.com>
2023-05-31 09:17:11 +0200

    Mise à jour du component navbar et du style de celui ci

--- components/navbar.php
+++ components/navbar.php
@@ -1,3 +1,16 @@
 <?php
     echo("ça marche");
 ?>
+
+<nav class="navbar">
+    <ul>
+        <li><nav id="mobile-nav"></nav></li>
+        <li><div class="nav logo"></div></li>
+        <li class="nav action">
+            <ul>
+                <li class="nav icon"><button><img href="./" alt="."></button></li>
+                <li class="nav icon"><button><img href="./" alt="."></button></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
