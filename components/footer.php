2d86166a5f43b4a340c368c24c59779b8532e108
Nils <65852707+Swannnils@users.noreply.github.com>
2023-05-31 09:55:04 +0200

    Changement du style du footer

--- components/footer.php
+++ components/footer.php
@@ -3,6 +3,6 @@
         <li><a href="./">Mentions légales</a></li>
         <li><a href="./">Politique de confidentialité</a></li>
         <li><a href="./">Conditions générales de vente</a></li>
-        <li>©Confiz 2023</li>
     </ul>
+    <p>©Confiz 2023</p>
 </footer>
