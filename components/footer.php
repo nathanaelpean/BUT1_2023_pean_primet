ee319c541bb970a00ff947b63956ff25c6960f55
Nils <65852707+Swannnils@users.noreply.github.com>
2023-05-31 09:42:24 +0200

    Création du component footer

---
+++ components/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <ul>
+        <li><a href="./">Mentions légales</a></li>
+        <li><a href="./">Politique de confidentialité</a></li>
+        <li><a href="./">Conditions générales de vente</a></li>
+        <li>©Confiz 2023</li>
+    </ul>
+</footer>
