4eaa677c1072cf0a52dcc20e110583399ff9bf0d
Nathanaël PÉAN <contact@nathanaelpean.fr>
2023-06-06 14:08:23 +0200

    sql update

--- index.php
+++ index.php
@@ -8,6 +8,14 @@ require_once($root . "./components/head.php");
         <?php require_once($root . '/components/navbar.php'); ?>
     </header>
     <main>
+        <div class="form-compact">
+            <form action="/boutiques" method="get">
+                <div class="box">
+                    <legend>titre</legend>
+                    <input type="text" name="search" required>
+                </div>
+            </form>
+        </div>
     </main>
     <?php require_once($root . '/components/footer.php'); ?>
 </body>
---
+++ test.php
@@ -0,0 +1,29 @@
+<style>
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+</style>
+
+<?php
+
+require_once("db.php");
+$test = requete("SELECT * FROM utilisateurs");
+
+echo ("<table>");
+foreach ($test as $t){
+    echo("<tr>");
+    echo ("<td>".$t["utilisateur_id"]."</td>");
+    echo ("<td>".$t["username"]."</td>");
+    echo ("<td>".$t["password"]."</td>");
+    echo ("<td>".$t["email"]."</td>");
+    echo ("<td>".$t["num_tel"]."</td>");
+    echo ("<td>".$t["role"]."</td>");
+    echo ("<td>".$t["nom"]."</td>");
+    echo ("<td>".$t["prenom"]."</td>");
+    echo ("<td>".$t["ddn"]."</td>");
+    echo("</tr>");
+}
+echo ("</table>");
+
+?>
