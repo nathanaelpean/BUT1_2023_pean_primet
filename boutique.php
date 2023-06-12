e396be543ee4bd7812c37c70d96828de7e41457c
Nils <65852707+Swannnils@users.noreply.github.com>
2023-06-12 17:47:36 +0200

    Style de boutique.php

--- boutique.php
+++ boutique.php
@@ -1,13 +1,13 @@
 
-        <div class="boutique card">
+        <div class="boutique-card">
             <h2>Nom de la marque</h2>
-            <p class="boutique info">3 confiseries disponibles</p>
-            <a class="boutique action">Aller à la boutique</a>
+            <p class="info">3 confiseries disponibles</p>
+            <a class="boutique-action">Aller à la boutique</a>
         </div>
-        <ul class="bonbon liste">
-            <li class="bonbon type">
-                <img class="bonbon miniature" src="https://images.unsplash.com/photo-1499195333224-3ce974eecb47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1051&q=80">
-                <div class="bonbon info">
+        <ul class="bonbon-liste">
+            <li class="bonbon-type">
+                <img class="bonbon-miniature" src="https://images.unsplash.com/photo-1499195333224-3ce974eecb47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1051&q=80">
+                <div class="bonbon-info">
                     <h2>Dragicamion</h2>
                     <p class="info">6.5euros/unité</p>
                 </div>
