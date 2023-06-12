92ac0138471c3b28cf84458488ef42e512dba402
Nathanaël PÉAN <contact@nathanaelpean.fr>
2023-06-12 23:18:10 +0200

    foreach boutiquess

--- boutiques-liste.php
+++ boutiques-liste.php
@@ -1,3 +1,16 @@
-<?php
+<div class="flex">
+    <div>
+    <?php
+    $boutiques = req("SELECT * FROM boutiques");
+    $adresses = req("SELECT * FROM adresses");
 
-?>
+    foreach($boutiques as $id -> $infos){ ?>
+        <a class="boutique" href="?id=<?= $id ?>">
+            <p><?php print_r($value) ?></p>
+        </a>
+    <?php } ?>
+    </div>
+    <div>
+        CARTE
+    </div>
+</div>
