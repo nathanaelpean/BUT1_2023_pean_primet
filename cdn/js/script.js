c8f7ea0f9f2c59efae4ec988936e6b7f7e6bfa27
Nathanaël PÉAN <contact@nathanaelpean.fr>
2023-06-21 17:31:12 +0200

    dashboard update

--- cdn/js/script.js
+++ cdn/js/script.js
@@ -4,3 +4,9 @@ const btnMenu = document.getElementById("burger-btn");
 btnMenu.addEventListener("click", function() {
         menuBurger.classList.toggle("active");
     });
+
+function popup(id,data){
+    let code="<div><form>"
+    data.forEach();
+    "</form></div>";
+}
--- dashboard.php
+++ dashboard.php
@@ -11,8 +11,45 @@ $user_data = user_data($_SESSION["id"]);
     </header>
     <main>
         <div class="flex ext">
-            <p>Bonjour <?= $user_data["prenom"] ?></p>
+            <?php if($_SESSION["role"] === "admin" || $_SESSION["role"] === "gerant"){ ?>
+                Vue : 
+                <form class="form_view" action="">
+                    <select name="view" class="select_view">
+                        <?php if($_SESSION["role"] === "admin"){ ?>
+                            <option value="admin">Administrateur</option>
+                        <?php } ?>
+                        <option value="gerant" <?php if(isset($_GET["view"]) && $_GET["view"] == "gerant" || $_SESSION["role"] === "gerant"){echo "selected";} ?>>Gérant</option>
+                        <option value="user" <?php if(isset($_GET["view"]) && $_GET["view"] == "user"){echo "selected";} ?>>Utilisateur</option>
+                    </select>
+                </form>
+                <script>
+                    document.querySelector(".select_view").addEventListener("change",e=>{
+                        document.querySelector(".form_view").submit();
+                    });
+                </script>
+            <?php } ?>
             <a href="deconnexion">Déconnexion</a>
+        </div>
+        <div>
+            <?php
+                if(isset($_GET["view"])){
+                    if($_GET["view"] == "admin" && $_SESSION["role"] == "admin"){
+                        require_once("./interfaces/admin.php");
+                    }elseif($_GET["view"] == "gerant" && ($_SESSION["role"] == "admin" || $_SESSION["role"] == "gerant")){
+                        require_once("./interfaces/gerant.php");
+                    }else{
+                        require_once("./interfaces/user.php");
+                    }
+                }else{
+                    if($_SESSION["role"] == "admin"){
+                        require_once("./interfaces/admin.php");
+                    }elseif($_SESSION["role"] == "gerant"){
+                        require_once("./interfaces/gerant.php");
+                    }else{
+                        require_once("./interfaces/user.php");
+                    }
+                }
+            ?>
         </div>
     </main>
     <?php require_once($root . '/components/footer.php'); ?>
