7ec6cfc46bd33e737d973101b4f9c6ac8310c2f0
Nathanaël PÉAN <contact@nathanaelpean.fr>
2023-06-23 16:59:06 +0200

    vue admin V2

--- interfaces/admin.php
+++ interfaces/admin.php
@@ -1,9 +1,10 @@
 Panneau à afficher : 
 <form class="form_section" action="">
     <select name="section" class="select_section">
-        <option value="users">Utilisateurs</option>
-        <option value="confiseries">Confiseries</option>
-        <option value="shops">Boutiques</option>
+        <option value="users" <?php if($_GET["section"] === "users"){echo "selected";} ?>>Utilisateurs</option>
+        <option value="confiseries" <?php if($_GET["section"] === "confiseries"){echo "selected";} ?>>Confiseries</option>
+        <option value="shops" <?php if($_GET["section"] === "shops"){echo "selected";} ?>>Boutiques</option>
+        <option value="adresses" <?php if($_GET["section"] === "adresses"){echo "selected";} ?>>Adresses</option>
     </select>
 </form>
 <script>
@@ -13,34 +14,50 @@ Panneau à afficher :
 </script>
 <?php 
     if(isset($_GET["section"]) && $_GET["section"] === "shops"){
-        $shops = req("SELECT * FROM boutiques;");
-        foreach($shops as $shop){ ?>
-        <form>
+        if(isset($_POST["update"])){
+            if($_POST["delete"]){
+                req("DELETE FROM boutiques WHERE boutique_id = '".$_POST["update"]."';");
+            }else{
+                $collected_data = array(
+                    $id => $_POST["update"],
+                    $adresse => $_POST["adresse"],
+                    $gerant => $_POST["gerant"]
+                );
+
+            }
+        }
+
+        foreach(req("SELECT * FROM boutiques;") as $shop){ ?>
+        <form method="post" action="dashboard?section=shops">
+            <input type="hidden" name="update" value="<?= $shop["boutique_id"] ?>">
             <h3><?= $shop["nom"] ?></h3>
             <p>Adresse : 
-            <?php
-                $adresse = req("SELECT * FROM adresses WHERE adresse_id = '".$shop["adresse_id"]."';")[0];
-                print_r($adresse);
-            ?>
+            <select name="adresse" id="">
+                <?php
+                    foreach(req("SELECT * FROM adresses") as $adresse){ ?>
+                        <option value="<?= $adresse["adresse_id"] ?>" <?php if($shop["adresse_id"] === $adresse["adresse_id"]){echo "selected";} ?>><?= $adresse["numero_rue"].",".$adresse["nom_adresse"]." ".$adresse["code_postal"]." ".$adresse["ville"] ?></option>
+                    <?php }
+                ?>
+            </select> 
             </p>
             <p>Gérant :
-                <?php
-                    $gerant = req("SELECT username FROM utilisateurs WHERE utilisateur_id = '".$shop["utilisateur_id"]."';")[0];
-                    print_r($gerant);
-                ?>
                 <select name="gerant" id="">
                 <?php
-                    foreach(req("SELECT username FROM utilisateurs;") as $user){
-                        echo $user["role"];
-                        // if($user["role"] === "gerant" || $user["role"] === "admin"){
-                        //  ?>
-                        <!--    <option value="<?= $user["utilisateur_id"] ?>"><?= $user["username"] ?></option>
-                            <?php -->
-                        // }
+                    foreach(req("SELECT * FROM utilisateurs;") as $user){
+                        if($user["role"] === "gerant" || $user["role"] === "admin"){
+                        ?>
+                            <option value="<?= $user["utilisateur_id"] ?>" <?php if($shop["utilisateur_id"] === $user["utilisateur_id"]){echo "selected";} ?>><?= $user["username"] ?></option>
+                        <?php
+                        }
                     }
                 ?>
                 </select> 
             </p>
+            <p>
+                <input type="checkbox" name="delete" id="del_<?= $shop["boutique_id"] ?>">
+                <label for="del_<?= $shop["boutique_id"] ?>">Supprimer cette boutique</label>
+            </p>
+            <button>Mettre à jour</button>
             </form>
         <?php }
     }elseif(isset($_GET["section"]) && $_GET["section"] === "confiseries"){
